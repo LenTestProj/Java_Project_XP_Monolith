a92b47be74c7380e53495741a9670504c5ff3359
LenTestProj <lennfernz2@gmail.com>
2026-08-02 22:29:48 +0530

    added changes

--- src/main/java/com/example/spring_xp_monolith/Services/JwtService2.java
+++ src/main/java/com/example/spring_xp_monolith/Services/JwtService2.java
@@ -1,27 +1,89 @@
 package com.example.spring_xp_monolith.Services;
 
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
 public class JwtService2 {
     private String secretKey;
 
-    public JwtService2(){
+    JwtService2(){
         this.secretKey = generateSecretKey();
     }
 
     public String generateSecretKey(){
-        try {
+        try{
             KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
             SecretKey secretKey = keyGen.generateKey();
+            System.out.println("Secret Key : " + secretKey.toString());
             return Base64.getEncoder().encodeToString(secretKey.getEncoded());
-        } catch (Exception e) {
-           throw new RuntimeException("Error generating secret key");
+        }
+        catch(NoSuchAlgorithmException e){
+            throw new RuntimeException("Error generating secret key", e);
         }
     }
     
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+
+        return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(username)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + 1000*60*3))
+        .signWith(getKey(), SignatureAlgorithm.HS256)
+        .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     
+   private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+        .setSigningKey(getKey())
+        .build().parseClaimsJws(token)
+        .getBody();   
+   }
+
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUserName(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+       final String username = extractUserName(token);
+       return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
 
 }
